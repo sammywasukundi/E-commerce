7ef38fff93f13f5f6890e79fd23acbc9f5bbac1d
sammywasukundi <brosammy1et2@gmail.com>
2024-05-08 17:54:25 +0200

    finalisation du panier

--- pages/basket.php
+++ pages/basket.php
@@ -1,5 +1,22 @@
 <?php
     include 'home.php';
+
+    if(isset($_POST['panier']['qte'])){
+        foreach ($_SESSION['panier'] as $product_id => $qte) {
+            $_SESSION['panier'][$product_id] = $_POST['panier']['qte'][$product_id];
+        }
+        recalculer();
+    }
+
+    function recalculer(){
+        $_SESSION['panier'] = $_POST['panier']['qte'];
+    }
+
+    // delete products
+    if(isset($_GET['del'])){
+        $id_del = $_GET['del'];
+        unset($_SESSION['panier'][$id_del]);
+    }
     
 ?>
 
@@ -13,70 +30,84 @@
     //     $show = $pdo->prepare($select);
     //     $show->execute();
 
-    //     $total = 0; // Initialisation du total
-
+    $total = 0; // Initialisation du total
+     
 ?>
 <div class="p-4 sm:ml-64">
     <div class="p-4 border-none rounded-lg dark:border-gray-700 mt-8">
-    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 mt-14">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-6 py-3"></th>
-                <th scope="col" class="px-6 py-3">Product name</th>
-                <th scope="col" class="px-6 py-3">Quantity</th>
-                <th scope="col" class="px-6 py-3">Price</th>
-                <th scope="col" class="px-6 py-3 text">Action</th>
-            </tr>
-        </thead>
-        <?php 
-            $ids = array_keys($_SESSION['panier']);
-            if(empty($ids)){
-                echo 'Votre panier est vide';
-            }else{
-                // $selects = $pdo->prepare('SELECT p.quantite, pr.nom, pr.prix, pr.img_produit
-                // FROM panier p INNER JOIN
-                // produits pr ON p.produit_id = pr.id_produit WHERE pr.id_produit IN ('.implode(',',$ids).')');
-                $selects = $pdo->prepare('SELECT * FROM produits WHERE id_produit IN ('.implode(',',$ids).')');
+        <form action="basket.php" method="post">
+            
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 mt-14">
+                
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <?= require 'model/message.php'; ?>
+                    <tr>
+                        <th scope="col" class="px-6 py-3"></th>
+                        <th scope="col" class="px-6 py-3">Product name</th>
+                        <th scope="col" class="px-6 py-3">Quantity</th>
+                        <th scope="col" class="px-6 py-3">Price</th>
+                        <th scope="col" class="px-6 py-3 text">Action</th>
+                    </tr>
+                </thead>
+                <?php 
+                    $ids = array_keys($_SESSION['panier']);
+                    if(empty($ids)){
+                        $_SESSION['message'] = 'Votre panier est vide !';
+                        // header('Location:basket.php');
+                        // exit(0);
+                    }else{
+                        // $selects = $pdo->prepare('SELECT p.quantite, pr.nom, pr.prix, pr.img_produit
+                        // FROM panier p INNER JOIN
+                        // produits pr ON p.produit_id = pr.id_produit WHERE pr.id_produit IN ('.implode(',',$ids).')');
+                        $selects = $pdo->prepare('SELECT * FROM produits WHERE id_produit IN ('.implode(',',$ids).')');
 
-                $selects->execute($ids);
-                foreach ($selects as $select) :
-        ?>
-        <tbody>
+                        $selects->execute($ids);
+                        foreach ($selects as $select) :
+                        $total += $select['prix'] * $_SESSION['panier'][$select['id_produit']];
 
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    <img src="img_produits/<?= $select['img_produit']; ?>" class="w-14 h-12 object-cover" alt="">
-                </th>
-                <td class="px-6 py-4"><?= $select['nom']; ?></td>
-                <td class="px-6 py-4"><?= $_SESSION['panier'][$select['id_produit']] ?></td>
-                <td class="px-6 py-4"><?= $select['prix']; ?></td>
-                <td class="px-6 py-4">
-                    <button class="font-medium rounded-lg text-sm px-5 py-2.5" type="submit">
-                        <a href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:text-red-400">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                            </svg>   
-                        </a>
-                    </button>
-                </td>    
-            </tr>
-            <?php 
-                endforeach;
-                    // Ajout du prix du produit au total
-                    //$total += $select['prix'] * $_SESSION['panier'][$select['id_produit']];
-                //}
-            }
-            ?>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"></th>
-                <td class="px-6 py-4"></td>
-                <td class="px-6 py-4"></td>
-                <td class="px-6 py-4 font-bold">
-                    Total : <?= '$total'; ?> <!-- Affichage du total -->
-                </td>
-                <td class="px-6 py-4"></td>
-            </tr>
-        </tbody>
-    </table>
+                ?>
+                <tbody>
+
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <img src="img_produits/<?= $select['img_produit']; ?>" class="w-14 h-12 object-cover" alt="">
+                        </th>
+                        <td class="px-6 py-4"><?= $select['nom']; ?></td>
+                        <td class="px-6 py-4">
+                            <input class="w-14 border-0 focus:ring-0" type="text" value="<?= $_SESSION['panier'][$select['id_produit']] ?>" name="panier[qte][<?= $select['id_produit']; ?>]">
+                        </td>
+                        <td class="px-6 py-4"><?= $select['prix'].' '; ?>$</td>
+                        <td class="px-6 py-4">
+                            <a href="basket.php?del=<?= $select['id_produit'] ?>">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                </svg>   
+                            </a>
+                        </td>    
+                    </tr>
+                    <?php 
+                        endforeach;
+                            // Ajout du prix du produit au total
+                        //}
+                    }
+                    ?>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"></th>
+                        <td class="px-6 py-4"></td>
+                        <td class="px-6 py-4">
+                            <button type="submit">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9 text-gray-600 hover:text-gray-800">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                                </svg>
+                            </button>
+                        </td>
+                        <td class="px-6 py-4 font-bold">
+                            Total : <?= $total; ?>$ <!-- Affichage du total -->
+                        </td>
+                        <td class="px-6 py-4"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
     </div>
 </div>
--- pages/products.php
+++ pages/products.php
@@ -5,11 +5,18 @@ if (!isset($_SESSION['ID_Utilisateur'])) {
     header('Location: index.php');
     exit(0);
 }
+// else{
+//     if ($_SESSION['role'] == 'admin') {
+//         require 'model/message.php';
+//         $_SESSION['message'] = 'Bonjour '.$_SESSION['first_name_user'].', vous êtes connectés entant qu\'administrateur';
+//     }    
+// }
+
 ?>
 
 
-<div class="p-4 sm:ml-64">
-    <div class="rounded-lg dark:border-gray-700 p-4">
+<div class="p-2 sm:ml-64">
+    <div class="rounded-lg dark:border-gray-700 p-2">
         <div class="grid lg:grid-cols-3 md:grid-cols-2 mt-14 gap-4 mb-4">
             <?php
             $select_product = 'SELECT * FROM produits';
@@ -73,25 +80,20 @@ if (!isset($_SESSION['ID_Utilisateur'])) {
                                         ?>
                                     </div>
                                 </div>
-                                    
-                                <div class="flex justify-end items-center">
-                                    <span
-                                        class="text-xl font-semibold text-gray-600"><?= $row['nom'] . ' ' . number_format($row['prix'], 2, '.', ' '); ?>$
-                                    </span>
-                                    <a href="Add_to_cart.php?id_produit=<?= $row['id_produit'] ?>"
-                                        class=" focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center text-gray-600 hover:text-gray-800">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                                        </svg>
-                                    </a>
-                                    <a href="Add_to_cart.php?id_produit=<?= $row['id_produit'] ?>"
-                                        class=" focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center text-gray-600 hover:text-gray-800">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                        </svg>
-                                    </a>
+                                <div class="flex justify-between items-center">
+                                    <span class="text-base font-normal text-gray-900 "
+                                    ><?= $row['nom'] ?><?= ' '.number_format($row['prix'],2,',',','); ?>$</span
+                                    >
+                                    <a
+                                    href="Add_to_cart.php?id_produit=<?= $row['id_produit'] ?>"
+                                    class="text-yellow-200 bg-white hover:text-gray-700 font-medium rounded-tl-lg rounded-bl-lg text-sm px-5 py-2.5 text-center"
+                                    ><ion-icon class="w-7 h-7" name="basket"></ion-icon></a
+                                    >
+                                    <a
+                                    href="Add_to_cart.php?id_produit=<?= $row['id_produit'] ?>"
+                                    class="text-yellow-200 bg-white hover:text-gray-700 font-medium rounded-tl-lg rounded-bl-lg text-sm px-5 py-2.5 text-center"
+                                    ><ion-icon name="trash" class="w-7 h-7"></ion-icon></a
+                                    >
                                 </div>
                             </div>
                         </div>
